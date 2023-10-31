657215412b860e5aae9125acf3260a660e5a168b
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-31 15:34:00 +0100

    added base algorithm

--- script/script.js
+++ script/script.js
@@ -3,9 +3,42 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
+        tasks : [
+            {
+                id:1,
+                text: 'pippo',
+                done: true
+            },
+            {
+                id:2,
+                text: 'pluto',
+                done: false
+            },
+            {
+                id: 3,
+                text: 'topolino',
+                done: false
+            },
+        ],
+        lastId : 3,
+        todoText:'',
       }
     },
     methods:{
+                
+        removeTask(i){
+            this.tasks.splice(i,1);
+        },
+        addTask(){
+            this.lastId++;
+            const newTask={
+                id: this.lastId,
+                text: this.todoText,
+                done: false,
+            };
+            this.tasks.unshift(newTask);
+            this.todoText = '';
+        },
     },
     amounted(){
     }
